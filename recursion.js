bd1bd1076f2d1304cee730b7e98c8c53da242a95
rachityadav <rachit.r.yadav@gmail.com>
2023-03-29 15:06:59 +0530

    recusion through arrow function

--- recursion.js
+++ recursion.js
@@ -1,11 +1,22 @@
 // when function all itself
 // take a example of basic power example
 
-function pow(x,n){
+
+// function pow(x,n){
+//     if (n==1){
+//         return x
+//     }else{
+//         return x*pow(x,n-1)
+//     }
+// }
+
+// run code through arrow funtion
+const pow=(x,n)=>{
     if (n==1){
         return x
     }else{
         return x*pow(x,n-1)
     }
 }
+
 console.log("pow(2,3)",pow(2,3))
