4d761068d51e331ce07d9f645919e3820ff5fd4b
rachityadav <rachit.r.yadav@gmail.com>
2023-03-30 13:21:13 +0530

    merge in new_branch branch

--- try_catch.js
+++ try_catch.js
@@ -3,7 +3,7 @@ try{
     let b =3
     c= a+b
     console.log("error not defned",c)
-    console.log("valuescheck",dfsf)
+    console.log("check which data you want to merge in main file",dfsf)
 }catch{
     console.log("varible is not defined")
 }
